ba90dd19ed81a6443b4dcc52176ace0d98a05733
svantehultmanhedin-glitch <svante.hultman.hedin2@gmail.com>
2026-04-08 23:12:17 +0200

    KV

---
+++ api/test-kv.js
@@ -0,0 +1,7 @@
+import { kv } from "./_kv";
+
+export default async function handler(req, res) {
+  await kv.set("hello", "world");
+  const value = await kv.get("hello");
+  res.json({ value });
+}
